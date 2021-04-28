87fa0c672870c7bfa1630643e5c490af60e77e63
saipratap99 <122005067@sastra.ac.in>
2021-04-28 21:32:17 +0530

    Complete time slot feat done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,37 @@ class UsersController < ApplicationController
 
   def ensure_user_logged_in_given_slot
     user = User.find(@current_user.id)
-    # if user.started_at
-    #   quizzes_curr = Quiz.where("start_time <= ? AND end_time >= ?", DateTime.now, DateTime.now)
-    # else
-    #   quizzes_curr = Quiz.where("start_time <= ? AND closing_time >= ?", DateTime.now, DateTime.now)
-    # end
-    quizzes_curr = Quiz.where("start_time <= ? AND end_time >= ?", DateTime.now, DateTime.now).first
+    if user.tech_1_started_at || user.tech_2_started_at || user.non_tech_1_started_at || user.non_tech_2_started_at
+      quizzes_curr = Quiz.where("start_time <= ? AND end_time >= ?", DateTime.now, DateTime.now)
+    else
+      quizzes_curr = Quiz.where("start_time <= ? AND  closing_time >= ?", DateTime.now, DateTime.now)
+    end
 
-    if quizzes_curr
+    if user.appears_for == "tech"
+      quiz = quizzes_curr.where(name: "Technical").first if !quizzes_curr.empty?
+    elsif user.appears_for == "non-tech"
+      quiz = quizzes_curr.where(name: "Non Technical").first if !quizzes_curr.empty?
+    else
+      quiz = quizzes_curr.first if !quizzes_curr.empty?
+    end
+
+    if quiz
+      quiz_type = quiz.name == "Technical" ? "tech" : "non-tech"
+      level = quiz.level
+      if quiz_type == "tech"
+        if level == 1
+          user.tech_1_started_at = DateTime.now
+        else
+          user.tech_2_started_at = DateTime.now
+        end
+      else
+        if level == 1
+          user.non_tech_1_started_at = DateTime.now
+        else
+          user.non_tech_2_started_at = DateTime.now
+        end
+      end
+      user.save!
       return true
     else
       # redirect
